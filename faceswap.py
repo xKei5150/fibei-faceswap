0c173effd344f350fcda62fd83f690b066ce0fdc
Von Maverick <vmaverick22@gmail.com>
2024-04-18 12:31:53 +0800

    Initial commit

---
+++ faceswap.py
@@ -0,0 +1,88 @@
+import numpy as np
+import cv2
+import insightface
+import gfpgan
+
+
+def load_image(image_path):
+    return cv2.imread(image_path)
+
+
+def face_detection(image, app):
+    return app.get(image)
+
+
+def face_swap(source_image, target_face, swapper):
+    return swapper.get(source_image, target_face, paste_back=True)
+
+
+def face_upscale(image, upscaler):
+    _, _, upscaled_image = upscaler.enhance(image, has_aligned=False, only_center_face=False)
+    return upscaled_image
+
+
+def process_frame(frame, app, swapper, upscaler):
+    faces = face_detection(frame, app)
+    processed_frames = []
+    for face in faces:
+        swapped_face = face_swap(frame, face, swapper)
+        upscaled_frame = face_upscale(swapped_face, upscaler)
+        processed_frames.append(upscaled_frame)
+    return processed_frames
+
+
+def process_video(video_path, output_video_path, app, swapper, upscaler):
+    cap = cv2.VideoCapture(video_path)
+    if not cap.isOpened():
+        print("Error: Could not open the video file.")
+        return
+
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    output_video = cv2.VideoWriter(output_video_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (width, height))
+    print("Started")
+
+    try:
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                print("Error: Could not read frame.")
+                break
+
+            processed_frames = process_frame(frame, app, swapper, upscaler)
+            for processed_frame in processed_frames:
+                output_video.write(processed_frame)
+
+            print(f"Processed {cap.get(cv2.CAP_PROP_POS_FRAMES)} out of {total_frames} frames")
+
+            if cv2.waitKey(1) == ord('q'):
+                break
+    finally:
+        cap.release()
+        output_video.release()
+        cv2.destroyAllWindows()
+
+
+def main():
+    source_image_path = 'source.jpg'
+    video_path = 'target-1080p.mp4'
+    output_video_path = 'output_video.mp4'
+
+    source_image = load_image(source_image_path)
+    app = insightface.app.FaceAnalysis(name='buffalo_l', det_size=(640, 640))
+    app.prepare(ctx_id=0)
+    source_faces = face_detection(source_image, app)
+
+    swapper = insightface.model_zoo.get_model('/models/inswapper_128.onnx',
+                                              download=False, download_zip=False)
+    upscaler = gfpgan.GFPGANer(model_path='/models/GFPGANv1.4.pth',
+                               upscale=1, arch='clean', bg_upsampler=None)
+
+    process_video(video_path, output_video_path, app, swapper, upscaler)
+
+
+if __name__ == "__main__":
+    main()
